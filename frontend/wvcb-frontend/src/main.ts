2c8d2647df684f3695243771a17c5bc6635f0801
Logan Miller <millerl1960@gmail.com>
2024-08-04 13:43:23 -0700

    Fixes routing

--- frontend/wvcb-frontend/src/main.ts
+++ frontend/wvcb-frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app-routing.module'; 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
